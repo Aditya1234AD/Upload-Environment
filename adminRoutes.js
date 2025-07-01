33858e5a1b8e67a51bbe87685cd21dcb7a61d5b5
Aditya1234AD <158497215+Aditya1234AD@users.noreply.github.com>
2025-07-01 15:37:46 +0530

    Delete adminRoutes.js

--- adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { isAdmin } = require('../middleware/authMiddleware');
-const User = require('../models/User');
-const router = express.Router();
-router.get('/dashboard', async (req, res) => {
-    const users = await User.find( );
-    res.render('adminDashboard', { users });
-});
-module.exports = router;
