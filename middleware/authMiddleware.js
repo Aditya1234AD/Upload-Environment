7d316dfe532d45fcd21d74141caeefb18ae5add1
Aditya1234AD <158497215+Aditya1234AD@users.noreply.github.com>
2025-07-01 13:33:14 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,9 @@
 
+exports.isAuthenticated = (req, res, next) => {
+    if (req.session.userId) return next();
+    res.redirect('/login');
+};
+exports.isAdmin = (req, res, next) => {
+    if (req.session.isAdmin) return next();
+    res.redirect('/login');
+};
