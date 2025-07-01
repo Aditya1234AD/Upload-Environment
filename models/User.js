dc8c76ed40c8cb8177f95fed060cfe32995b902f
Aditya1234AD <158497215+Aditya1234AD@users.noreply.github.com>
2025-07-01 15:06:28 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    photo: String,
+    details: {
+        name: String,
+        village: String,
+        district: String
+    },
+    isAdmin: { type: Boolean, default: false }
+});
+module.exports =
+    mongoose.model('User', userSchema);
