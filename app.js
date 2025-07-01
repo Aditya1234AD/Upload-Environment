8c8af64d5e264429991b8118279f31bb571aea76
Aditya1234AD <158497215+Aditya1234AD@users.noreply.github.com>
2025-07-01 16:45:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(session({
     saveUninitialized: false,
     store: MongoStore.create({
         mongoUrl:
-            "mongodb+srv://AD123:<db_password>@cluster0.kx2yqsx.mongodb.net/";
+            "mongodb+srv://AD123:<db_password>@cluster0.kx2yqsx.mongodb.net/"
     })
 }));
 app.set('view engine', 'ejs');
