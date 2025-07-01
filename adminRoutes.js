2676764338f1280b5c419815d8dae1006d10a100
Aditya1234AD <158497215+Aditya1234AD@users.noreply.github.com>
2025-07-01 15:32:33 +0530

    Update adminRoutes.js

--- adminRoutes.js
+++ adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { isAdmin } = require('../.vscode/client-management/middleware/authMiddleware');
-const User = require('../.vscode/client-management/models/User');
+const { isAdmin } = require('../middleware/authMiddleware');
+const User = require('../models/User');
 const router = express.Router();
 router.get('/dashboard', async (req, res) => {
     const users = await User.find( );
