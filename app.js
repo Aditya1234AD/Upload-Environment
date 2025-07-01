e5a9e05062742b32e28f49d7866fa58d2c5d5c98
Aditya1234AD <158497215+Aditya1234AD@users.noreply.github.com>
2025-07-01 15:48:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const authRoutes = require('./authRoutes');
 const clientRoutes = require('./routes/clientRoutes');
 const adminRoutes = require('./routes/adminRoutes');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('./middleware/authMiddleware');
 const app = express();
 // DB connection
 mongoose.connect('mongodb://localhost:27017/userUploads');
